a40796d281a720782bb5779be0425791ff34cbd3
Dominik Röschke <dominik.roeschke@cloud37.io>
2022-09-23 15:06:44 +0200

    Added Simple Explanatory Svelte Component at the start

---
+++ src/components/SimpleComponent.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let number = 0;
+</script>
+
+<style>
+    .red {
+        color: red
+    }
+</style>
+
+<p>The number <span class:red={number === 5}>{number}</span> has been passed as a prop</p>
